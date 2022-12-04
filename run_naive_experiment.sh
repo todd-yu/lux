19d6b48dabab93e8c8dec8f03ce8f7880956470b
Todd Yu <toddyu@berkeley.edu>
2022-12-03 23:36:43 -0800

    updated experiment script

--- run_naive_experiment.sh
+++ run_naive_experiment.sh
@@ -1,34 +1,16 @@
 # ======= Naive Lux workloads, no optimizations =======
 
 # # no optimizations, 50k
-python3 naive_lux_workload.py --num-trials 30 --log-file-path "./experiment_logs/mqo_50k.txt" --data-file-path "./data/AB_NYC_2019.csv"
+python3 granular_mqo_workload.py --num-trials 10 --log-file-path "./experiment_logs/granular_mqo_50k.txt" --data-file-path "./data/AB_NYC_2019.csv" --topk
 
 # # no optimizations, 250k
-python3 naive_lux_workload.py --num-trials 30 --log-file-path "./experiment_logs/mqo_250k.txt" --data-file-path "./data/250k.csv"
+python3 granular_mqo_workload.py --num-trials 10 --log-file-path "./experiment_logs/granular_mqo_250k.txt" --data-file-path "./data/250k.csv" --topk
 
 # # no optimizations, 500k
-python3 naive_lux_workload.py --num-trials 30 --log-file-path "./experiment_logs/mqo_500k.txt" --data-file-path "./data/500k.csv"
+python3 granular_mqo_workload.py --num-trials 10 --log-file-path "./experiment_logs/granular_mqo_500k.txt" --data-file-path "./data/500k.csv" --topk
 
-# # no optimizations, 1m
-python3 naive_lux_workload.py --num-trials 30 --log-file-path "./experiment_logs/mqo_1m.txt" --data-file-path "./data/1m.csv"
+# # no optimizations, 10
+python3 granular_mqo_workload.py --num-trials 10 --log-file-path "./experiment_logs/granular_mqo_1m.txt" --data-file-path "./data/1m.csv" --topk
 
 # # no optimizations, 5m
-python3 naive_lux_workload.py --num-trials 30 --log-file-path "./experiment_logs/mqo_5m.txt" --data-file-path "./data/5m.csv"
-
-
-# ======= Naive Lux workloads, sampling + prune (all current opt) =======
-
-# all optimizations, 50k
-python3 naive_lux_workload.py --num-trials 30 --log-file-path "./experiment_logs/mqo_all_opt_50k.txt" --data-file-path "./data/AB_NYC_2019.csv" --topk --sampling
-
-# all optimizations, 250k
-python3 naive_lux_workload.py --num-trials 30 --log-file-path "./experiment_logs/mqo_all_opt_250k.txt" --data-file-path "./data/250k.csv" --topk --sampling
-
-# all optimizations, 500k
-python3 naive_lux_workload.py --num-trials 30 --log-file-path "./experiment_logs/mqo_all_opt_500k.txt" --data-file-path "./data/500k.csv" --topk --sampling
-
-# all optimizations, 1m
-python3 naive_lux_workload.py --num-trials 30 --log-file-path "./experiment_logs/mqo_all_opt_1m.txt" --data-file-path "./data/1m.csv" --topk --sampling
-
-# all optimizations, 5m
-python3 naive_lux_workload.py --num-trials 30 --log-file-path "./experiment_logs/mqo_all_opt_5m.txt" --data-file-path "./data/5m.csv" --topk --sampling
+python3 granular_mqo_workload.py --num-trials 10 --log-file-path "./experiment_logs/granular_mqo_5m.txt" --data-file-path "./data/5m.csv" --topk
