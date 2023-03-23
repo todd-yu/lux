fba8bb2d880adef06a17544a8149dee22a043eac
Todd Yu <toddyu@berkeley.edu>
2023-03-23 09:52:52 -0700

    added fixed col

---
+++ run_incr_col.sh
@@ -0,0 +1,7 @@
+python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_16_incremental_500ops.txt" --data-file-path "./data/crimedata_16.csv" --num-ops 500
+
+python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_32_incremental_500ops.txt" --data-file-path "./data/crimedata_32.csv" --num-ops 500
+
+python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_32_incremental_500ops.txt" --data-file-path "./data/crimedata_64.csv" --num-ops 500
+
+python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_32_incremental_500ops.txt" --data-file-path "./data/crimedata.csv" --num-ops 500
