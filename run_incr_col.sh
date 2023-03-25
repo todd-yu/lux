9c697abb2fd7216608188c955e29b02cb856bfd9
Todd Yu <toddyu@berkeley.edu>
2023-03-25 16:33:07 -0700

    update topk to 250 in fixed eval

--- run_incr_col.sh
+++ run_incr_col.sh
@@ -1,7 +1,7 @@
-python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_16_incremental_500ops.txt" --data-file-path "./data/crimedata_16.csv" --num-ops 500
+python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_16_incremental_500ops_topk_250.txt" --data-file-path "./data/crimedata_16.csv" --num-ops 500
 
-python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_32_incremental_500ops.txt" --data-file-path "./data/crimedata_32.csv" --num-ops 500
+python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_32_incremental_500ops_topk_250.txt" --data-file-path "./data/crimedata_32.csv" --num-ops 500
 
-python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_64_incremental_500ops.txt" --data-file-path "./data/crimedata_64.csv" --num-ops 500
+python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_64_incremental_500ops_topk_250.txt" --data-file-path "./data/crimedata_64.csv" --num-ops 500
 
-python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_128_incremental_500ops.txt" --data-file-path "./data/crimedata.csv" --num-ops 500
+python3 fixed_eval.py --num-trials 3 --log-file-path "./fixed_incr_experiment_logs/crimedata_128_incremental_500ops_topk_250.txt" --data-file-path "./data/crimedata.csv" --num-ops 500
